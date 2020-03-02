6d2151b7b87bd98ba51e3b9ddc40edc22fa234f3
vijay-jindal <vijay123jindal@gmail.com>
2020-03-02 21:05:05 +0530

    Basic project structure

---
+++ flaskr/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/dataset')
+    def dataset():
+        return "Dataset"
+
+
+    return app
