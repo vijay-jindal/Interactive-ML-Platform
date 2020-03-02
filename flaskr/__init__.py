0a2efebd5089786bacea4e4afe4326dcb11d251c
vijay-jindal <vijay123jindal@gmail.com>
2020-03-02 22:29:22 +0530

    Simplify app run

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,40 +1,32 @@
 import os
 from flask import Flask,request
 
+# create the app
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+@app.route('/')
+def hello():
+    return "Welcome to Interactive ML platform! Please create a project"
+   #return render_template('template_name.html')
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return "Welcome to Interactive ML platform! Please create a project"
-       #return render_template('template_name.html')
-
-    @app.route('/newproject',methods=['POST','GET'])
-    def newproject():
-        if request.method == 'GET':
-            return "Enter project Name and select the Dataset"
-            #return render_template('template_name.html')
-        if request.method == 'POST':
-            """Check whether the file exists and whether it is in csv format
-               Store the dataset if valid in fs
-               If above conditions don't satisfy, show exception
-               like 'The file format or the project name are invalid' (show a dialog box)
-               If above conditions are statisfied, then create a new directory"""
-            return "Validating Dataset"
-            #return render_template('template_name.html,var1=value,var2=value)
-
-    @app.route('/preprocess',methods=['POST','GET'])
-    def preprocess():
+@app.route('/newproject',methods=['POST','GET'])
+def newproject():
+    if request.method == 'GET':
         return "Enter project Name and select the Dataset"
-        #return render_template('template_name.html',var1=value)
+        #return render_template('template_name.html')
+    if request.method == 'POST':
+        """Check whether the file exists and whether it is in csv format
+           Store the dataset if valid in fs
+           If above conditions don't satisfy, show exception
+           like 'The file format or the project name are invalid' (show a dialog box)
+           If above conditions are statisfied, then create a new directory"""
+        return "Validating Dataset"
+        #return render_template('template_name.html,var1=value,var2=value)
 
-    return app
+@app.route('/preprocess',methods=['POST','GET'])
+def preprocess():
+    return "Enter project Name and select the Dataset"
+    #return render_template('template_name.html',var1=value)
+
+if __name__ == '__main__':
+    app.run(debug=True)
