6ebefa53dde4033301a31489f9d4b87fe37e8457
vijay-jindal <vijay123jindal@gmail.com>
2020-03-02 22:12:24 +0530

    initial API paths added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
+from flask import Flask,request
 
-from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
@@ -13,13 +13,28 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
-        return 'Hello, World!'
+        return "Welcome to Interactive ML platform! Please create a project"
+       #return render_template('template_name.html')
 
-    @app.route('/dataset')
-    def dataset():
-        return "Dataset"
+    @app.route('/newproject',methods=['POST','GET'])
+    def newproject():
+        if request.method == 'GET':
+            return "Enter project Name and select the Dataset"
+            #return render_template('template_name.html')
+        if request.method == 'POST':
+            """Check whether the file exists and whether it is in csv format
+               Store the dataset if valid in fs
+               If above conditions don't satisfy, show exception
+               like 'The file format or the project name are invalid' (show a dialog box)
+               If above conditions are statisfied, then create a new directory"""
+            return "Validating Dataset"
+            #return render_template('template_name.html,var1=value,var2=value)
 
+    @app.route('/preprocess',methods=['POST','GET'])
+    def preprocess():
+        return "Enter project Name and select the Dataset"
+        #return render_template('template_name.html',var1=value)
 
     return app
