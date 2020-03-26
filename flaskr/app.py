ac6022979de02df67ff09b09da091d597b0d5c4b
Sharan Varma <sharanvarma0@gmail.com>
2020-03-26 21:23:47 +0530

    JSON Handoff Followed - Dashboard, Upload dataset

--- flaskr/app.py
+++ flaskr/app.py
@@ -4,10 +4,11 @@ Refer to this : https://flask.palletsprojects.com/en/1.1.x/logging/ and
 https://docs.python.org/3/library/logging.html#logging-levels
 Ex: app.logger.info("Message")
 """
-
+Project_name = ""
 from flask import (
-    Flask, render_template, request, redirect
+    Flask, render_template, request, redirect, url_for
 )
+import requests
 from services.project import Project
 
 # create the app
@@ -23,35 +24,27 @@ app.logger.setLevel(logging.INFO)
 def imlp():
     return render_template('dashboard.html')
 
-@app.route('/project/<project_name>')
-def new_project(project_name):
-    project.create(project_name)
-    return redirect('/project/'+project_name+'/upload')
+@app.route('/project',methods=['POST'])
+def new_project():
+    Project_name = request.form['project-name']
+    Project_name = project.create(Project_name)
+    return redirect('/upload/'+Project_name)
 
-@app.route('/project/<project_name>/upload',methods=['GET','POST'])
+@app.route('/upload/<project_name>',methods=['GET'])
 def upload(project_name):
-    if request.method == 'GET':
-        if hasattr(project,"name") and project_name == getattr(project,"name"):
-            return render_template('upload-csv.html')
-        else:
-            return redirect('/')
-    elif request.method == 'POST' and 'dataset' in request.files:
-        project.upload_dataset(request.files['dataset']) # allow only csv files in UI
-        # TODO : create instance of dataset class
-        return redirect('/project/'+project_name+'/preprocess')
-    else:
-        return redirect('/')
+    return render_template('upload-csv.html',proj_name=project_name)
+    
 
-@app.route('/project/<project_name>/preprocess')
-def preprocess(project_name):
-    if request.method == 'GET':
-        if hasattr(project,"name") and project_name == getattr(project,"name") and hasattr(project,"dataset"):
+@app.route('/preprocess',methods=['POST'])
+def preprocess():
+    if request.form['dataset'] is not null:
+            project.upload_dataset(request.form['dataset'])
             column_names,data_types = project.dataset.info()
             app.logger.info(column_names)
             app.logger.info(data_types)
             app.logger.info(project.dataset.path)
-            return render_template('preprocess.html',path=project.dataset.path,column_names=column_names,data_types=data_types)
-        else:
+            return render_template('preprocess.html',project_name=Project_name,path=project.dataset.path,column_names=column_names,data_types=data_types)
+    else:
             return redirect('/')
     return "WIP"
 
